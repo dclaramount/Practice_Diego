48f83f5d270e4ded188b87012fc892c7ce5c5790
dclaramount <diegoaclaramount@gmail.com>
2022-07-27 10:38:07 +0200

    Ciclic Rotation (easy) Lesson 2

--- Diego_Practice/Program.cs
+++ Diego_Practice/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Diego is Here Diego Diego");
+using Diego_Practice;
 
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] A = new int[] { 3, 8, 9, 7, 6 };
+        int K = 3;
+        int[] result = Array_Lesson2.Excercise(A, K);
+    }
+}
